915053a6b0070e0b29f6dcb1aea51af296e01bd6
whatvvv <92416239+whatvvv@users.noreply.github.com>
2022-03-03 22:46:19 +0800

    类

---
+++ 未命名5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+	public:
+		void study();
+		void exam();
+		string _name;
+		int _age;
+};
+void Student::study()
+{
+	cout<<"learn C++"<<endl;
+}
+void Student::exam()
+{
+	cout<<"C++ sore is 100"<<endl;
+}
+int main()
+{
+	Student stu;
+	stu._name="zhangsan";
+	stu._age=-20;
+	cout<<stu._name<<stu._age<<"sui"<<endl;
+	stu.study();
+	stu.exam();
+	return 0;
+}
